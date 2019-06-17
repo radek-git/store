d4aa32928c0fcc332fa0855fff3e3f72c63b8dfc
radek-git <istlud.klint@gmail.com>
2019-06-17 23:25:11 +0200

    controllers

--- src/main/java/com/radek/store/controllers/ProductController.java
+++ src/main/java/com/radek/store/controllers/ProductController.java
@@ -1,13 +1,12 @@
 package com.radek.store.controllers;
 
 import com.radek.store.dto.ProductDTO;
+import com.radek.store.entity.Product;
 import com.radek.store.mapper.ProductMapper;
 import com.radek.store.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,4 +39,19 @@ public class ProductController {
     public List<ProductDTO> getProductByBrandAndName(@PathVariable String brand, @PathVariable String name) {
         return productMapper.toDTO(productService.findByBrandAndName(brand, name));
     }
+
+    @PostMapping("/products")
+    public ProductDTO postProduct(@RequestBody Product product) {
+        return productMapper.toDTO(productService.save(product));
+    }
+
+//    @PatchMapping("/products/{name}")
+//    public ProductDTO update(@PathVariable String name,  @RequestBody Product product) {
+//
+//    }
+
+//    @DeleteMapping("/products/{name}")
+//    public ResponseEntity<Object> deleteByName(@PathVariable String name) {
+//
+//    }
 }
