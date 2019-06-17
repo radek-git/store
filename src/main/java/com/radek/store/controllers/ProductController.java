8127970ff1a3b659e5c8dd7dbd5ead3947ecd3bd
radek-git <istlud.klint@gmail.com>
2019-06-17 23:32:59 +0200

    controllers

--- src/main/java/com/radek/store/controllers/ProductController.java
+++ src/main/java/com/radek/store/controllers/ProductController.java
@@ -5,7 +5,6 @@ import com.radek.store.entity.Product;
 import com.radek.store.mapper.ProductMapper;
 import com.radek.store.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
