41e0b0f1cd226c2ffc663be62b19db4f8a97a386
radek-git <istlud.klint@gmail.com>
2019-06-25 22:54:01 +0200

    security

---
+++ src/main/java/com/radek/store/security/UserDetailsImpl.java
@@ -0,0 +1,53 @@
+package com.radek.store.security;
+
+import com.radek.store.entity.users.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !user.isExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !user.isLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !user.isCredentialsExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
---
+++ src/main/java/com/radek/store/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.radek.store.security;
+
+
+import com.radek.store.entity.users.User;
+import com.radek.store.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service("userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException());
+
+        return new UserDetailsImpl(user);
+    }
+}
