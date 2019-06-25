6ecda204ef1532d0c8d9b8b86439e835e3faa760
radek-git <istlud.klint@gmail.com>
2019-06-25 18:07:14 +0200

    security annotations

---
+++ src/main/java/com/radek/store/ApiResponse.java
@@ -0,0 +1,19 @@
+package com.radek.store;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse {
+
+    private int status;
+    private String message;
+
+
+}
