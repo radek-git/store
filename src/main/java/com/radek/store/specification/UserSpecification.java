54fda97050027b8e56d63710e63b8ee97ff95528
radek-git <istlud.klint@gmail.com>
2019-06-28 23:00:43 +0200

    @Getter and @Setter instead of @Data

--- src/main/java/com/radek/store/specification/UserSpecification.java
+++ src/main/java/com/radek/store/specification/UserSpecification.java
@@ -1,7 +1,6 @@
 package com.radek.store.specification;
 
-
-import com.radek.store.entity.users.Customer;
+import com.radek.store.entity.users.User;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.And;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
@@ -11,11 +10,7 @@ import org.springframework.data.jpa.domain.Specification;
 @And({
         @Spec(path = "name", spec = Equal.class),
         @Spec(path = "surname", spec = Equal.class),
-        @Spec(path = "username", spec = Equal.class),
-        @Spec(path = "city", spec = Equal.class),
-        @Spec(path = "street", spec = Equal.class),
-        @Spec(path = "postalCode", spec = Equal.class),
-
+        @Spec(path = "username", spec = Equal.class)
 })
-public interface CustomerSpecification extends Specification<Customer> {
+public interface UserSpecification extends Specification<User> {
 }
